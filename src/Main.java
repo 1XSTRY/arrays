63b360559dee69f83e0a6185e7f6336f283e876b
1XSTRY <kosogornov99@gmail.com>
2023-10-08 21:33:30 +0500

    complete task3 and fix task 1 and 2 (task2 is completed in the same section as task1)

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,61 @@
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        task1();
-        //task2();
-        //task3();
+        task1_2();
+        task3();
         //task4();
     }
-    public static void task1 (){
-        int[] weight = new int[12];
-        weight [0] = 1;
-        weight [1] = 2;
-        weight [2] = 3;
-        for (int i = 0; i < 3; i++) {
+
+    public static void task1_2() {
+        int[] weight = new int[3];
+        weight[0] = 1;
+        weight[1] = 2;
+        weight[2] = 3;
+        System.out.print(Arrays.toString(weight));
+
+        System.out.println();
+
+        double[] weight2 = {1.57, 7.654, 9.986};
+        System.out.println(Arrays.toString(weight2));
+
+        int[] weight3 = {1, 4, 9, 16, 25, 36};
+        System.out.println(Arrays.toString(weight3));
+
+    }
+
+    public static void task3() {
+        int[] weight = new int[3];
+        weight[0] = 1;
+        weight[1] = 2;
+        weight[2] = 3;
+        for (int i = weight.length - 1; i >= 0; i--) {
             System.out.print(weight[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+
+
         }
         System.out.println();
 
         double[] weight2 = {1.57, 7.654, 9.986};
-        for (double value : weight2) {
-            System.out.printf("%.2f ", value);
+        for (int i = weight2.length - 1; i >= 0; i--) {
+            System.out.print(weight2[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
         }
         System.out.println();
 
         int[] weight3 = {1, 4, 9, 16, 25, 36};
-        for (int p : weight3) {
-            System.out.printf("%d ", p);
+        for (int i = weight3.length - 1; i >= 0; i--) {
+            System.out.print(weight3[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
         }
 
 
-
-
-
-
     }
+
 }
